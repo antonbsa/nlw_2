0693b87221da7e33f4ab37c5921f87df47be1ad2
Anton <antonbsa@gmail.comAnton>
2020-08-17 21:10:07 -0300

    day 5

---
+++ src/database/createProffy.js
@@ -0,0 +1,78 @@
+module.exports = async function(db,{ proffyValue, classValue, classScheduleValues}){
+    // inserir dados na table de teacher
+    const insertedProffy = await db.run(`
+        INSERT INTO proffys (
+            name,
+            avatar,
+            whatsapp,
+            bio
+        ) VALUES (
+            "${proffyValue.name}",
+            "${proffyValue.avatar}",
+            "${proffyValue.whatsapp}",
+            "${proffyValue.bio}"
+        );
+    `)
+    
+    const proffy_id = insertedProffy.lastID
+    
+    //inserir dados na tabela classes
+    const insertedClass = await db.run(`
+            INSERT INTO classes (
+                subject,
+                cost,
+                proffy_id
+            ) VALUES (
+                "${classValue.subject}",
+                "${classValue.cost}",
+                "${proffy_id}"
+             );
+    `)
+    
+    const class_id = insertedClass.lastID
+    
+    // Inserir dados na tabela class_schedule
+    const insertedAllClassScheduleValues = classScheduleValues.map((classScheduleValue) => {
+        return db.run(`
+            INSERT INTO class_schedule (
+                class_id,
+                weekday,
+                time_from,
+                time_to
+            ) VALUES (
+                "${class_id}",
+                "${classScheduleValue.weekday}",
+                "${classScheduleValue.time_from}",
+                "${classScheduleValue.time_to}"
+            );
+        `)
+    })
+    // aqui vou executar todos os db.run() das class schedule
+    
+    // await Promise.all(insertedAllClassScheduleValues)
+
+    // Consultar os dados inseridos
+
+    // todos os proffys
+    const selectedProffys = await db.all("SELECT * FROM proffys")
+    // console.log(selectedProffys)
+
+    // consultar as classes de um determinado professor e trager junto os dados dele
+    const selectClassesAndProffys = await db.all(`
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE classes.proffy_id = 1;
+    `)
+    // console.log(selectClassesAndProffys)
+
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = 0
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520"
+    `)
+    console.log(selectClassesSchedules)
+}
